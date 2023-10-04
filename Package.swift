b06ea8661f9a86451665e8936fcff9f0a5052256
Skander Ben Abdelmalak <skander@iubenda.com>
2023-10-04 03:33:56 +0200

    ios 12 target

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "CmpSdk",
     platforms: [
-        .iOS(.v11) // or the appropriate platform
+        .iOS(.v12) // or the appropriate platform
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
