e9948ea31b9ee7becf2f237c8102677a0a436965
Skander Ben Abdelmalak <skander@iubenda.com>
2024-01-17 13:36:20 +0100

    1.99.3

---
+++ CmpSdk.xcframework/ios-arm64/CmpSdk.framework/Headers/CmpGoogleConsentMap.h
@@ -0,0 +1,13 @@
+//
+//  CmpGoogleConsentMap.h
+//  CmpSdk
+//
+//  Created by Skander Ben Abdelmalak on 14.01.24.
+//
+
+#ifndef CmpGoogleConsentMap_h
+#define CmpGoogleConsentMap_h
+
+typedef NSDictionary<NSString *, NSString *> CMPConsentMap;
+
+#endif /* CmpGoogleConsentMap_h */
