52d879e094477fea129c6bebe8e94d6af7a45121
Skander Ben Abdelmalak <skander@iubenda.com>
2024-02-19 22:42:54 +0100

    refactor: public api for ATT Manager

---
+++ CmpSdk.xcframework/ios-arm64/CmpSdk.framework/Headers/CmpATTrackingManager.h
@@ -0,0 +1,30 @@
+//
+//  CmpATTrackingManager.h
+//  CmpSdk
+//
+//  Created by Skander Ben Abdelmalak on 15.12.23.
+//
+
+#ifndef CmpATTrackingManager_h
+#define CmpATTrackingManager_h
+#import <AppTrackingTransparency/AppTrackingTransparency.h>
+#import <Foundation/Foundation.h>
+#import <CmpSdk/CmpEventListenerTypes.h>
+
+API_AVAILABLE(ios(14))
+@interface CmpATTrackingManager : NSObject
+
+@property (nonatomic, copy) CmpATTrackingStatusChangedListener statusChangeCallback;
+
++ (instancetype)sharedManager;
+- (void)requestATTPermissionWithCompletion:(void (^)(ATTrackingManagerAuthorizationStatus status))completion API_AVAILABLE(ios(14));
+- (ATTrackingManagerAuthorizationStatus)getStoredATTStatus API_AVAILABLE(ios(14));
+- (ATTrackingManagerAuthorizationStatus)getCurrentATTStatus API_AVAILABLE(ios(14));
+- (NSDate *)getLastUpdatedTimestamp;
+- (BOOL)isATTPromptShown;
+- (NSDictionary *)checkATTStatusChange;
+- (BOOL)hasATTStatusChanged API_AVAILABLE(ios(14));
+- (void)storeATTStatus:(ATTrackingManagerAuthorizationStatus)status  API_AVAILABLE(ios(14));
+@end
+
+#endif /* CmpATTrackingManager_h */
